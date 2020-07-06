cb0a392ca47f465d920189c5df8c80b16fc74a27
Carla Regina Rodrigues <carla.rodrigues@programmers.com.br>
2020-07-06 17:05:00 -0300

    Angular9

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,26 @@
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  products: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action']
+
+  constructor(private service: ProductService) { }
+
+  ngOnInit(): void {
+    this.service.read().subscribe(products => {
+      this.products = products;
+      console.log(products);
+      
+    })
+
+  }
+
+}
---
+++ frontend/src/app/components/template/header/header-data.model.ts
@@ -0,0 +1,7 @@
+import { StringifyOptions } from 'querystring';
+
+export interface HeaderData {
+    title: string,
+    icon: string,
+    routeUrl: string
+}
